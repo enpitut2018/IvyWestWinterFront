2639f00a994ca763f4cd71b23d77da909258289c
guri3 <aloesquash.lock@gmail.com>
2018-10-07 00:33:26 +0900

    react-native-vector-iconsのエラーを解消

--- android/app/src/main/java/com/ivywestwinterfront/MainApplication.java
+++ android/app/src/main/java/com/ivywestwinterfront/MainApplication.java
@@ -3,7 +3,6 @@ package com.ivywestwinterfront;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.oblador.vectoricons.VectorIconsPackage;
 import org.reactnative.camera.RNCameraPackage;
 import org.reactnative.camera.RNCameraPackage;
 import com.facebook.react.ReactNativeHost;
@@ -26,7 +25,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new VectorIconsPackage(),
             new RNCameraPackage(),
             new RNCameraPackage()
       );

e1352491bd4398bd2886265db02b56cce52e5ede
Yuto Koguchi <10llip0p@kurokuro.local>
2018-11-15 23:46:29 +0900

    downloads画面を実装

--- src/components/DownloadPhotosScreen/index.js
+++ src/components/DownloadPhotosScreen/index.js
@@ -1,12 +1,61 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  ScrollView,
+  Dimensions
+} from "react-native";
+import AutoHeightImage from "react-native-auto-height-image";
+import { getFetchWithToken } from "../../models/fetchUtil";
+import { baseURL } from "../../libs/const";
+
+const { width } = Dimensions.get("window");
 
 export default class DownloadPhotosScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      photos: []
+    };
+  }
+
+  reloadPhoto() {
+    url = baseURL + "/downloads";
+    getFetchWithToken(url).then(json => {
+      this.setState({
+        photos: json
+      });
+    }).catch(error => {
+      console.log(error);
+    });
+  }
+
+  componentWillMount() {
+    this.reloadPhoto();
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.sample}>DownloadPhotosScreen</Text>
-      </View>
+      <ScrollView style={styles.container}>
+        <View style={styles.photoView}>
+          {this.state.photos.map((photo, index) => {
+            return (
+              <AutoHeightImage
+                key={index}
+                width={width / 3}
+                source={{ uri: photo.Url }}
+              />
+            );
+          })}
+        </View>
+        <Button
+          onPress={() => this.reloadPhoto()}
+          title="Reload"
+          color="#841584"
+        />
+      </ScrollView>
     );
   }
 }
@@ -14,9 +63,12 @@ export default class DownloadPhotosScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
     backgroundColor: "#F5FCFF"
+  },
+  photoView: {
+    flex: 3,
+    flexDirection: "row",
+    flexWrap: "wrap"
   },
   sample: {
     fontSize: 20,

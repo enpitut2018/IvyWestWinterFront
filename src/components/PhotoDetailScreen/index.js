d0f68ecc701ad771b15ed94af0bd225729f65f13
a_team <aloesquash.lock@gmail.com>
2018-11-17 21:14:37 +0900

    画像詳細にユーザIDを表示(内容はサンプル)

--- src/components/PhotoDetailScreen/index.js
+++ src/components/PhotoDetailScreen/index.js
@@ -1,5 +1,12 @@
 import React, { Component } from "react";
-import { Dimensions, ScrollView } from "react-native";
+import {
+  Dimensions,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View
+} from "react-native";
 import AutoHeightImage from "react-native-auto-height-image";
 
 const { width } = Dimensions.get("window");
@@ -13,7 +20,17 @@ class PhotoDetailScreen extends Component {
     const { photo } = this.props;
 
     return (
-      <ScrollView>
+      <ScrollView style={styles.container}>
+        <View style={styles.userInfo}>
+          <Image
+            style={styles.userAvatar}
+            source={{
+              uri:
+                "https:/s3-ap-northeast-1.amazonaws.com/ivy-west-winter/user-face-photos/bfn7ucj3spn4isqqr1bg.jpg"
+            }}
+          />
+          <Text style={styles.userId}>guri3</Text>
+        </View>
         <AutoHeightImage width={width} source={{ uri: photo.Url }} />
       </ScrollView>
     );
@@ -21,3 +38,28 @@ class PhotoDetailScreen extends Component {
 }
 
 export default PhotoDetailScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: "#F5FCFF"
+  },
+  userInfo: {
+    flex: 1,
+    height: 50,
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "white"
+  },
+  userAvatar: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    marginRight: 10,
+    marginLeft: 10
+  },
+  userId: {
+    fontSize: 15
+  }
+});

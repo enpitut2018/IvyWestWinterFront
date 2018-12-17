6626b70fb151121cbbd9ee372133eb71dafda577
a_team <aloesquash.lock@gmail.com>
2018-12-17 21:07:33 +0900

    画像サムネイルが表示される前の背景色を設定&サムネイル間のマージンを設定

--- src/components/common/TouchablePhoto.js
+++ src/components/common/TouchablePhoto.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Image, TouchableOpacity } from "react-native";
 import { Actions } from "react-native-router-flux";
 
+const photoMarginSize = 1;
+
 const TouchablePhoto = props => {
   const { photo, width, height } = props;
 
@@ -11,7 +13,12 @@ const TouchablePhoto = props => {
     <TouchableOpacity onPress={onPressPhoto}>
       <Image
         key={photo.ID}
-        style={{ width: width, height: height }}
+        style={{
+          width: width - photoMarginSize * 2,
+          height: height - photoMarginSize * 2,
+          backgroundColor: "#EEEEEE",
+          margin: photoMarginSize
+        }}
         source={{ uri: photo.url }}
       />
     </TouchableOpacity>

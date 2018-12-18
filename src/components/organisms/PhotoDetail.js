a0d1da432aeb2fcf4bfbe81f742cc16ca907dba5
a_team <aloesquash.lock@gmail.com>
2018-12-18 16:50:06 +0900

    画像詳細のリファクタリング

--- src/components/organisms/PhotoDetail.js
+++ src/components/organisms/PhotoDetail.js
@@ -3,19 +3,31 @@ import { Dimensions, View } from "react-native";
 import UserInfo from "../parts/UserInfo";
 import PeopleAvatarArea from "../parts/PeopleAvatarArea";
 import AutoHeightImage from "react-native-auto-height-image";
+import { createImageProgress } from "react-native-image-progress";
+import * as Progress from "react-native-progress";
 
 // 画面サイズを取得
-const { width } = Dimensions.get("window");
+const { width, height } = Dimensions.get("window");
 
-const TimeLineCard = ({ photo }) => {
+const ImageWithProgress = createImageProgress(AutoHeightImage);
+
+const PhotoDetail = ({ photo, photoWidth, photoHeight }) => {
   return (
-    <View style={{ marginBottom: 10 }}>
+    <View>
       <UserInfo
         avatarURL="https:/s3-ap-northeast-1.amazonaws.com/ivy-west-winter/user-face-photos/bfn7ucj3spn4isqqr1bg.jpg"
         userID="guri3"
         // TODO きちんとしたデータに差し替える
       />
-      <AutoHeightImage width={width} source={{ uri: photo.url }} />
+      <ImageWithProgress
+        style={{
+          width: photoWidth * (width / photoWidth),
+          height: photoHeight * (width / photoWidth)
+        }}
+        width={width}
+        source={{ uri: photo.url }}
+        indicator={Progress.Pie}
+      />
       <PeopleAvatarArea
         photoID={photo.id}
         people={"test"}
@@ -25,4 +37,4 @@ const TimeLineCard = ({ photo }) => {
   );
 };
 
-export default TimeLineCard;
+export default PhotoDetail;

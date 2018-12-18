9cb18956f5686b591b963fb9bf6dbd477c26c570
a_team <aloesquash.lock@gmail.com>
2018-12-18 10:48:38 +0900

    タイムラインの表示

--- src/components/screens/DownloadTimeLineScreen.js
+++ src/components/screens/DownloadTimeLineScreen.js
@@ -1,18 +1,8 @@
 import React, { Component } from "react";
-import {
-  Dimensions,
-  Image,
-  RefreshControl,
-  ScrollView,
-  StyleSheet,
-  Text,
-  View
-} from "react-native";
+import { RefreshControl, ScrollView, StyleSheet } from "react-native";
+import TimeLineCard from "../parts/TimeLineCard";
 import { getFetchWithToken } from "../../models/fetchUtil";
 import { baseURL } from "../../libs/const";
-
-// 画面サイズを取得
-const { width } = Dimensions.get("window");
 
 class DownloadTimeLineScreen extends Component {
   constructor(props) {
@@ -73,18 +63,9 @@ class DownloadTimeLineScreen extends Component {
           />
         }
       >
-        <View style={styles.userInfo}>
-          <Image
-            style={styles.userAvatar}
-            source={{
-              uri:
-                "https:/s3-ap-northeast-1.amazonaws.com/ivy-west-winter/user-face-photos/bfn7ucj3spn4isqqr1bg.jpg"
-            }}
-            // TODO サンプル画像差し替え
-          />
-          {/* <Text style={styles.userId}>{photos[0].userid}</Text> */}
-        </View>
-        <Image style={{ width: width }} source={{ uri: photos[0].url }} />
+        {photos.map((photo, index) => {
+          return <TimeLineCard photo={photo} />;
+        })}
       </ScrollView>
     );
   }

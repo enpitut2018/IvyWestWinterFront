b74df4ded9d9d8d6f63a45293af844c32547d5e2
a_team <aloesquash.lock@gmail.com>
2018-12-19 12:11:42 +0900

    タイムラインから写真詳細へ行けるようにした

--- src/components/organisms/TimeLineCard.js
+++ src/components/organisms/TimeLineCard.js
@@ -1,15 +1,21 @@
-import React, { Component } from "react";
-import { Dimensions, View } from "react-native";
+import React from "react";
+import { Dimensions, TouchableOpacity, View } from "react-native";
 import UserInfo from "../parts/UserInfo";
 import PeopleAvatarArea from "../parts/PeopleAvatarArea";
 import AutoHeightImage from "react-native-auto-height-image";
+import { Actions } from "react-native-router-flux";
 
 // 画面サイズを取得
 const { width } = Dimensions.get("window");
 
 const TimeLineCard = ({ photo }) => {
+  const onPressTimeLineCard = () => Actions.photoDetail({ photo: photo });
+
   return (
-    <View style={{ marginBottom: 10 }}>
+    <TouchableOpacity
+      style={{ marginBottom: 10 }}
+      onPress={onPressTimeLineCard}
+    >
       <UserInfo
         avatarURL="http://jiyuubito21102.com/wp-content/uploads/2018/02/yosiokariho.jpg.pagespeed.ce.Shw1B9OFrq.jpg"
         userID="yoshioka"
@@ -21,7 +27,7 @@ const TimeLineCard = ({ photo }) => {
         people={"test"}
         // TODO きちんとしたデータに差し替える
       />
-    </View>
+    </TouchableOpacity>
   );
 };
 

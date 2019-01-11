ce30341cc7284e2abc430236196414106947063d
a_team <aloesquash.lock@gmail.com>
2019-01-11 15:36:18 +0900

    定数を更新

--- src/libs/const.js
+++ src/libs/const.js
@@ -1,3 +1,3 @@
-export const baseURL = "https://ivy-west-winter-test.herokuapp.com";
+export const baseURL = "https://ivy-west-winter.herokuapp.com";
 // export const baseURL = "http://localhost:8080"; // 開発用
 export const asyncStorageKeyPrefix = "@IvyWest:";
